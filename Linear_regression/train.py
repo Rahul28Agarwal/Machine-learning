bd201fa7b59cd41350a2ac7ca843e4d752bfa1ed
rahul agarwal <rahulagg1997@gmail.com>
2023-01-09 15:52:26 +1100

    add regularization functions

--- Linear_regression/train.py
+++ Linear_regression/train.py
@@ -1,32 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import linear_regression as lr
-import stochastic as slr
+import linear_regression_algorithm as lr
 
 np.random.seed(42)
 n_samples = 100
 X = 2* np.random.rand(n_samples,1)
 y = 4 + 3* X + np.random.randn(n_samples, 1)
 
-fig=plt.figure()
+# fig=plt.figure()
 
-ax=fig.add_axes([0,0,1,1])
-ax.grid(alpha=0.5)
-ax.scatter(X, y, color='b')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_title('Randomly generated linear dataset')
-ax.set_ylim(ymin=0)
+# ax=fig.add_axes([0,0,1,1])
+# ax.grid(alpha=0.5)
+# ax.scatter(X, y, color='b')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_title('Randomly generated linear dataset')
+# ax.set_ylim(ymin=0)
 
-plt.show()
+# plt.show()
 
-
-model = lr.LinearRegression()
-model.fit(X,y)
-print("model works")
-print(model.get_weights_bias())
-# model.predict([])
-
-slr_model = slr.StochasticGradientDescent()
-slr_model.fit(X,y)
-print(slr_model.get_weights_bias())
+model1 = lr.LinearRegression(n_epochs=5000)
+model1.gradient_descent_fit(X,y)
+print(model1.get_weights_bias())
+model2 = lr.LinearRegression(n_epochs=50)
+model2.stochastic_gradient_descent_fit(X,y)
+print(model2.get_weights_bias())
+model1.ridge_GD_fit(X,y)
+print(model1.get_weights_bias())
+model1.lasso_GD_fit(X,y)
+print(model1.get_weights_bias())
+model1.elastic_net_GD_fit(X,y)
+print(model1.get_weights_bias())
